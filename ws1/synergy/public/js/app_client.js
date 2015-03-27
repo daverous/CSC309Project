f109f52871c61ea8fdd8aa3682b316587f232211
Babneet Singh <babneet.singh@mail.utoronto.ca>
2015-03-27 17:59:05 -0400

    Adding angular infra

---
+++ ws1/synergy/public/js/app_client.js
@@ -0,0 +1,14 @@
+var app = angular.module('myApp', ['ui.bootstrap']);
+
+app.controller('HomeController', ['$scope', '$http', '$location', '$window',
+                                function($scope, $http, $location, $window) {
+
+
+}]);
+
+
+app.controller('TempController', ['$scope', '$http', '$location', '$window',
+                                function($scope, $http, $location, $window) {
+
+
+}]);                                  
