bd9f6f3604048f2c7647d8460b8d63b2b8ae05d2
David Wilson <dlw5@st-andrews.ac.uk>
2015-03-27 18:40:35 -0400

    fix

--- ws1/synergy/js/rentalManager.js
+++ ws1/synergy/js/rentalManager.js
@@ -198,40 +198,4 @@ module.exports = {
             update.save();
         });
     }
-    $(function() {
-        var source = $("#search-results").html();
-        var dataTemplate = Handlebars.compile(source);
-        $results = $('#results')
-
-        $('#search').on('keyup', function(e) {
-            if (e.keyCode === 13) {
-                var parameters = {
-                    search: $(this).val()
-                };
-                $.get('/searching', parameters, function(data) {
-                    if (data instanceof Array) {
-                        $results.html(dataTemplate({
-                            resultsArray: data
-                        }));
-                    } else {
-                        $results.html(data);
-                    };
-                });
-            };
-        });
-        $('#results').on('click', '.save-btn', function() {
-            var jobTitle = $(this).next('a').text()
-            var jobURL = $(this).next('a').attr('href')
-            var parameters = {
-                title: jobTitle,
-                url: jobURL
-            };
-            console.log(parameters)
-            $(this).parent().remove()
-            $.get('/save', parameters, function(data) {
-                $('#alert').html(data)
-                console.log(data)
-            });
-        });
-    });
 }
